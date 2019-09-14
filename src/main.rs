b546a75742d12f719834b84125a1dc2e5fcf8468
onoderis <onoderis@users.noreply.github.com>
2019-09-15 02:08:38 +0300

    Fix args indexes

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use serde::Deserialize;
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let user = &args[0];
-    let project = &args[1];
+    let user = &args[1];
+    let project = &args[2];
 
     let url = format!("https://api.github.com/repos/{}/{}/releases", user, project);
     let response: Vec<Release> = reqwest::get(&url)?
