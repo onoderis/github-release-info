3993042932983f72f6d527b35d409cac5887cbdd
onoderis <onoderis@users.noreply.github.com>
2019-11-24 14:17:29 +0300

    Add clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
+extern crate clap;
+extern crate core;
 extern crate reqwest;
 extern crate serde;
 
-use std::env;
+use core::fmt;
+use std::{env, error};
 use std::error::Error;
+use std::fmt::Display;
+use std::ops::Deref;
+use std::string::ParseError;
 
+use clap::{App, Arg, ArgMatches, SubCommand};
 use serde::Deserialize;
+use serde::export::Formatter;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Github release info")
+        .version("0.1.1") //todo version from cargo
+        .arg(Arg::with_name("user")
+            .short("u")
+            .long("user")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("repo")
+            .short("r")
+            .long("repo")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
 
-    let user = &args[1];
-    let project = &args[2];
+    let user = matches.value_of("user").ok_or(NoCliArgumentError)?;
+    let repository = matches.value_of("repo").ok_or(NoCliArgumentError)?;
 
-    let url = format!("https://api.github.com/repos/{}/{}/releases", user, project);
+    let url = format!("https://api.github.com/repos/{}/{}/releases", user, repository);
     let response: Vec<Release> = reqwest::get(&url)?
         .json()?;
 
@@ -31,4 +51,19 @@ struct Release {
 struct Asset {
     name: String,
     download_count: u32
+}
+
+#[derive(Debug, Clone)]
+struct NoCliArgumentError;
+
+impl fmt::Display for NoCliArgumentError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "No required cli argument")
+    }
+}
+
+impl error::Error for NoCliArgumentError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
 }
